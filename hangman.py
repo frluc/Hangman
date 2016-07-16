1d89d137453c7806b1ff020aa0bf55d573d57329
frluc <frluc.perso+github@gmail.com>
2016-07-16 16:40:27 +0200

    add basic string & list manipulation

--- hangman.py
+++ hangman.py
@@ -1,3 +1,37 @@
 #Hangman
 
-print('-- HANGMAN --')
+import random
+
+#defining functions
+
+def getword():
+	word = "testword"
+	return word
+	
+#variables initialization
+
+maxtries = 6
+guesses = ''
+	
+#main
+
+print("-- HANGMAN --")
+print("Guess the word letter by letter, don't let that poor dude hang")
+
+tries = maxtries
+wordtoguess = list(getword().upper())
+
+print("Enter a letter")
+letterguess = input().upper()
+
+worddisplay = wordtoguess
+
+for i in range(len(worddisplay)):
+	if worddisplay[i] == letterguess:
+		print("true")
+		worddisplay[i] = letterguess
+	else:
+		print("false")
+		worddisplay[i] = "#"
+
+print(' '.join(worddisplay))
